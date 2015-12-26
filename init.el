3fa19bbdaac67b40f9177e4aa53e3ecac316e065
Ron <ronll64@gmail.com>
2015-12-26 12:43:38 +0200

    Removed duplicate code, seperated sections

--- init.el
+++ init.el
@@ -88,7 +88,12 @@
 (setq visible-bell nil)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
+;; -------------------- magit --------------------
 (require 'magit)
 (global-set-key "\C-xg" 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
@@ -108,49 +113,7 @@
  ac-use-menu-map t
  ac-candidate-limit 20)
 
-;; ;; Python mode settings
-(require 'python-mode)
-(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
-(setq py-electric-colon-active t)
-(add-hook 'python-mode-hook 'autopair-mode)
-(add-hook 'python-mode-hook 'yas-minor-mode)
-
-;; ;; Jedi settings
-(require 'jedi)
-;; It's also required to run "pip install --user jedi" and "pip
-;; install --user epc" to get the Python side of the library work
-;; correctly.
-;; With the same interpreter you're using.
-
-;; if you need to change your python intepreter, if you want to change it
-;; (setq jedi:server-command
-;;       '("python2" "/home/andrea/.emacs.d/elpa/jedi-0.1.2/jediepcserver.py"))
-
-(add-hook 'python-mode-hook
-	  (lambda ()
-	    (jedi:setup)
-	    (jedi:ac-setup)
-            (local-set-key "\C-cd" 'jedi:show-doc)
-            (local-set-key (kbd "M-SPC") 'jedi:complete)
-            (local-set-key (kbd "M-.") 'jedi:goto-definition)))
-
-
-(add-hook 'python-mode-hook 'auto-complete-mode)
-
-(ido-mode t)
-
-;; -------------------- extra nice things --------------------
-;; use shift to move around windows
-(windmove-default-keybindings 'shift)
-(show-paren-mode t)
- ; Turn beep off
-(setq visible-bell nil)
-
-(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
-(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "S-C-<down>") 'shrink-window)
-(global-set-key (kbd "S-C-<up>") 'enlarge-window)
-
+;; -------------------- helm --------------------
 (require 'helm)
 (require 'helm-config)
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
